e38ad1171a6752cc64d974602f3f0f112d804861
Anukool Patel <151703654+anukool23@users.noreply.github.com>
2025-07-22 00:14:03 +0530

    Adding button to handle acordian

--- src/components/MenuCategory.jsx
+++ src/components/MenuCategory.jsx
@@ -1,14 +1,19 @@
+import { use, useState } from "react"
 import ItemList from "./ItemList"
 
 const MenuCategory = (data) =>{
-    console.log(data)
+    const [showItems, setShowItems] = useState(false);
+
+    const handleClick = () =>{
+        setShowItems(!showItems);    
+    }
     return(<div>
     <div className="w-6/12 mx-auto my-2 bg-gray-100 shadow-lg p-2">
-        <div className="flex justify-between">
+        <div className="flex justify-between" onClick={handleClick}>
             <span className="font-bold">{data?.data?.title} ({data?.data?.itemCards.length})</span>
             <span>⬇️</span>
         </div>
-        <ItemList items={data?.data?.itemCards}/>
+        {showItems && <ItemList items={data?.data?.itemCards}/>}
         </div>
         </div>
     )
